e08083abde2c1d8642c000e2091c1e180beec70f
Adrian Ensslin <adrian_ensslin@web.de>
2025-05-07 12:12:06 +0200

    added drf-yasg

--- videoflix_config/urls.py
+++ videoflix_config/urls.py
@@ -19,13 +19,30 @@ from django.urls import path, include
 from django.conf.urls.static import static 
 from django.conf import settings  
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Videoflix API",
+      default_version='v1',
+      description="API Dokumentation für Videoflix",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('auth_app.api.urls')),
     path('api/movie/', include('movie_app.api.urls')),
     path('__debug__', include('debug_toolbar.urls')),
-    path('django-rq/', include('django_rq.urls'))
+    path('django-rq/', include('django_rq.urls')),
+
+    # Swagger und Redoc
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + staticfiles_urlpatterns()  
 
 if settings.DEBUG: 
