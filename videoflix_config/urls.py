7a264c5afa6fbdfc01b017c67e01abc2dba7e9f8
Adrian Ensslin <adrian_ensslin@web.de>
2025-05-07 13:16:34 +0200

    create 404 page

--- videoflix_config/urls.py
+++ videoflix_config/urls.py
@@ -52,3 +52,8 @@ urlpatterns = [
 
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+def custom_404(request, exception):
+    return render(request, "404.html", status=404)
+
+handler404 = 'videoflix_config.urls.custom_404'
