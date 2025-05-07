9daef812fca0dd29672aed23275cb596edc326bc
Adrian Ensslin <adrian_ensslin@web.de>
2025-05-07 13:10:54 +0200

    create welcome page

--- videoflix_config/urls.py
+++ videoflix_config/urls.py
@@ -22,6 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.shortcuts import render
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -33,7 +34,11 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+def api_home(request):
+    return render(request, 'api_home.html')
+
 urlpatterns = [
+    path('', api_home),
     path('admin/', admin.site.urls),
     path('api/auth/', include('auth_app.api.urls')),
     path('api/movie/', include('movie_app.api.urls')),
